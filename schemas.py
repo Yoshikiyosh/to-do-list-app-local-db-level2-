593281db6b90e99591eeb4ed49f04b3978e9b73d
Jack86-222 <yoshiki.kikuchi@nvt.jp>
2025-08-10 14:14:42 +0900

    fix of adding __pycache to .gitignore and delete class which is not needed in schemas.py

--- schemas.py
+++ schemas.py
@@ -7,10 +7,3 @@ class ToDoBase(BaseModel):
 class ToDoCreate(ToDoBase):
     """ToDo作成時のスキーマ"""
     pass
-
-class ToDo(ToDoBase):
-    """DB からのToDoレスポンススキーマ"""
-    id: int
-
-    class Config:
-        from_attributes = True
